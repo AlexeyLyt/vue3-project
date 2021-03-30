95b3324c8771aababf3a43e4f30e2e187b9f73b6
AlexeyLyt <katowicel333@gmail.com>
2021-03-30 14:51:22 +0300

    init

---
+++ dist/precache-manifest.22a92856c35ddf40cb51e39431cf260c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7131b627a77da494aef",
+    "url": "/css/app.37adbbe3.css"
+  },
+  {
+    "revision": "32ed7e085176a893ed3fcff8d8572662",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7131b627a77da494aef",
+    "url": "/js/app.cf10f581.js"
+  },
+  {
+    "revision": "9a945f330d5e6e011a68",
+    "url": "/js/chunk-2d0a4783.b026419e.js"
+  },
+  {
+    "revision": "a053265840b35da396ca",
+    "url": "/js/chunk-2d0be70b.c307066b.js"
+  },
+  {
+    "revision": "c80069a16b36b7579b28",
+    "url": "/js/chunk-2d216dc7.821ef3f4.js"
+  },
+  {
+    "revision": "50978e8288cd469aa402",
+    "url": "/js/chunk-vendors.735c58f5.js"
+  },
+  {
+    "revision": "f11bce428d38fd9f4d6f0ba31c2acffc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
